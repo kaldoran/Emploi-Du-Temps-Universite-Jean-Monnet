2bdafe3b33063791fd81637d194a40331f0d742e
Chocobozzz <djidane14ff@hotmail.fr>
2014-03-04 23:18:16 +0100

    nouveau fichier: img/ico.png         modifié:         index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 <html>
 <head>
     <meta charset="utf-8" />
+    <link rel="icon" type="image/png" href="img/ico.png">
     
     <style>
         body{
@@ -13,6 +14,11 @@
             font-size: 12px;
             font-weight: bold;
             margin: 0;
+            text-align: center;
+        }
+        
+        nav {
+            margin-bottom: 20px;
         }
         
         #info {
@@ -47,159 +53,146 @@
             margin-top: 100px;
             margin-bottom: 100px;
         }
+        
+        a {
+            text-decoration: none;
+            color: #4E99B8;
+            font-style: italic;
+        }
+        
+        a:hover{
+            color: orange;
+        }
     </style>
     
     <title>Emploi du temps Université Jean-Monnet (Saint-Etienne)</title>
 </head>
-<body style="text-align: center;">
+<body>
 
-<?php
-    // Récupération du code du lien sans quoi on ne pourrait pas voir l'image de l'edt
-    //exec("./code_edt.pl", $output);
-    //$contenu = $output[0];
-    
-    //if(!$contenu)
-    //    echo "<p cpass='error'>Problème avec la récupération de la clé</p>";
-    
-    //$code = $contenu;
-?>
-    
-    <label for="code_promo">Promo :</label>
-    <select id="code_promo">
-        <option value='3215'>L1 SVTE</option>
-        <option value='5844'>L1 Staps</option>
-        <option value='3797'>L1 Sc. & Techno</option>
-        <option value='3243%2C3244%2C3245%2C3246'>L2 Bio</option>
-        <option value='3235'>L2 Info</option>
-        <option value='3231'>L2 Math</option>
-        <option value='5788'>L2 Sc. Terre & Envi</option>
-        <option value='6677'>L2 Staps</option>
-        <option value='5790'>L3 Sc. Ingé</option>
-        <option value='3277'>L3 Bio</option>
-        <option value='3275'>L3 Chimie</option>
-        <option value='3083' selected="selected">L3 Info</option>
-        <option value='4760'>L3 Mass</option>
-        <option value='4762'>L3 Math</option>
-        <option value='3274'>L3 Physique</option>
-        <option value='2562'>L3 Phy. & Chimi</option>
-        <option value='4361'>L3 SVTE</option>
-        <option value='3293'>M1 Info</option>
-        <option value='3284'>M1 Math</option>
-    </select>
-    
-    <label for="jour">Jours :</label>
-<?php
-
-    /* Les jours de la semaine */
-    $array = array("Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi");
-    $day = date("w") - 1; /* -1 Car on veut pas dimanche ( dimanche = 0 ) Du coup la Lundi = 0 [ pratique pour le tableau au dessus ] */
-    
-    echo '<select id="jour" size="6" multiple="multiple">';
-    
-    /* Pour tout les jours de la semaine de 0 a 5 car le dimanche n'est pas dispo sur l'emploi du temps */
-    for($i = 0; $i <= 5; $i++) {
-        echo "<option value='$i'";
-    
-        if(($day == -1 && $i == 0) || $i == $day) /* Si on est dimanche on le met a lundi, sinon on sélectionne le jour actuel */
-            echo ' selected="selected"'; 
-            
-        echo ">";
+    <!-- Menu de navigation -->
+    <nav>
+        <label for="code_promo">Promo :</label>
+        <select id="code_promo">
+            <option value='3215'>L1 SVTE</option>
+            <option value='5844'>L1 Staps</option>
+            <option value='3797'>L1 Sc. & Techno</option>
+            <option value='3243%2C3244%2C3245%2C3246'>L2 Bio</option>
+            <option value='3235'>L2 Info</option>
+            <option value='3231'>L2 Math</option>
+            <option value='5788'>L2 Sc. Terre & Envi</option>
+            <option value='6677'>L2 Staps</option>
+            <option value='5790'>L3 Sc. Ingé</option>
+            <option value='3277'>L3 Bio</option>
+            <option value='3275'>L3 Chimie</option>
+            <option value='3083' selected="selected">L3 Info</option>
+            <option value='4760'>L3 Mass</option>
+            <option value='4762'>L3 Math</option>
+            <option value='3274'>L3 Physique</option>
+            <option value='2562'>L3 Phy. & Chimi</option>
+            <option value='4361'>L3 SVTE</option>
+            <option value='3293'>M1 Info</option>
+            <option value='3284'>M1 Math</option>
+        </select>
         
-        echo $array[$i] . "</option>";
-    }
-    
-    echo '</select>';
-?>
-    
-    <label for="semaine">Semaine :</label>
-    <input type="button" id="semaine_precedente" value="<" />
-    
-    <select id="semaine">
+        <label for="jour">Jours :</label>
     <?php
     
-        // Retourne la date correspondant au lundi de de la semaine $semaine et de l'année $annee
-        function premierlundi($semaine, $annee) {
-            if($semaine <= WEEK_START_NB)
-                $annee++;
+        /* Les jours de la semaine */
+        $array = array("Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi");
+        $day = date("w") - 1; /* -1 Car on veut pas dimanche ( dimanche = 0 ) Du coup la Lundi = 0 [ pratique pour le tableau au dessus ] */
+        
+        echo '<select id="jour" size="6" multiple="multiple">';
+        
+        /* Pour tout les jours de la semaine de 0 a 5 car le dimanche n'est pas dispo sur l'emploi du temps */
+        for($i = 0; $i <= 5; $i++) {
+            echo "<option value='$i'";
+        
+            if(($day == -1 && $i == 0) || $i == $day) /* Si on est dimanche on le met a lundi, sinon on sélectionne le jour actuel */
+                echo ' selected="selected"'; 
                 
-            // Timestamp de l'année jours 1 - mois 1
-            $begin = mktime(0, 0, 0, 1, 1, $annee);
+            echo ">";
             
-            // On décale de $semaine par rapport au debut de l'année
-            $offset = strtotime("+$semaine weeks", $begin);
-        
-            if (date('w', $offset) != 1) // Si c'est un lundi , on a gagné :p
-                $offset = strtotime("last monday", $offset); // On utilise la puissance du php et on se deplace au lundi
-        
-            return date('d-m', $offset);
+            echo $array[$i] . "</option>";
         }
-    
-        /* Pourquoi -1 , car l'année débute scolaire commence au milieu de l'année [ ex : Debut aout 2013 ]
-         * Du coup les semaines sont comptées à partir de la première date du début des cours
-         * Ex : Debut de l'emploi du temps la semaine 33 donc la semaine 33 [ semaine réelle ] est égale a 0
-         * Sur l'emploi du temps, on décale alors tout depuis cette date
-         */
-        $date = date("Y") - 1;
-        $num_semaine = strftime("%U");
         
-        for ($i = 0 ; $i <= NUMBER_WEEK_IN_YEAR ; $i++) {
-            $val = WEEK_START_NB + $i;
-            
-            if( $val > NUMBER_WEEK_IN_YEAR )
-                $val = $i - (NUMBER_WEEK_IN_YEAR - WEEK_START_NB);
-                
-            if($num_semaine + ( NUMBER_WEEK_IN_YEAR - WEEK_START_NB) == $i) {
-                $sv_num_semaine = $i;
-                echo "<option value='$i' selected='selected'>";
-            }
-            else {
-                echo "<option value='$i'>";
-            }
-        
-            echo premierlundi($val, $date);
-            
-            echo "</option>";
-        }
+        echo '</select>';
     ?>
+        
+        <label for="semaine">Semaine :</label>
+        <input type="button" id="semaine_precedente" value="<" />
+        
+        <select id="semaine">
+        <?php
+        
+            // Retourne la date correspondant au lundi de de la semaine $semaine et de l'année $annee
+            function premierlundi($semaine, $annee) {
+                if($semaine <= WEEK_START_NB)
+                    $annee++;
+                    
+                // Timestamp de l'année jours 1 - mois 1
+                $begin = mktime(0, 0, 0, 1, 1, $annee);
+                
+                // On décale de $semaine par rapport au debut de l'année
+                $offset = strtotime("+$semaine weeks", $begin);
+            
+                if (date('w', $offset) != 1) // Si c'est un lundi , on a gagné :p
+                    $offset = strtotime("last monday", $offset); // On utilise la puissance du php et on se deplace au lundi
+            
+                return date('d-m', $offset);
+            }
+        
+            /* Pourquoi -1 , car l'année débute scolaire commence au milieu de l'année [ ex : Debut aout 2013 ]
+             * Du coup les semaines sont comptées à partir de la première date du début des cours
+             * Ex : Debut de l'emploi du temps la semaine 33 donc la semaine 33 [ semaine réelle ] est égale a 0
+             * Sur l'emploi du temps, on décale alors tout depuis cette date
+             */
+            $date = date("Y") - 1;
+            $num_semaine = strftime("%U");
+            
+            for ($i = 0 ; $i <= NUMBER_WEEK_IN_YEAR ; $i++) {
+                $val = WEEK_START_NB + $i;
+                
+                if( $val > NUMBER_WEEK_IN_YEAR )
+                    $val = $i - (NUMBER_WEEK_IN_YEAR - WEEK_START_NB);
+                    
+                if($num_semaine + ( NUMBER_WEEK_IN_YEAR - WEEK_START_NB) == $i) {
+                    $sv_num_semaine = $i;
+                    echo "<option value='$i' selected='selected'>";
+                }
+                else {
+                    echo "<option value='$i'>";
+                }
+            
+                echo premierlundi($val, $date);
+                
+                echo "</option>";
+            }
+        ?>
+        
+        </select>
+        
+        <input type="button" id="semaine_suivante" value="&gt;" />
+    </nav>
     
-    </select>
-    
-    <input type="button" id="semaine_suivante" value="&gt;" />
-    
-    <br /><br />
-    
+    <!-- Image de l'emploi du temps ou du chargement -->
     <img alt="Emploi du temps" id="image" class="pacman" src="img/pacman.gif">
+        
     
-    <p>Code grandement inspiré de <a href="https://github.com/kaldoran/web/blob/master/ujm_emploi_du_temps.php" title="merci">Nicolas Reynaud</a></p>
+    <!-- Pied de page -->
+    <footer>
+        <p>
+            <a href="https://github.com/Chocobozzz/Emploi-du-Temps-Jean-Monnet-Web" title="Voir le dépot GitHub">Code</a>
+            grandement inspiré de
+            <a href="https://github.com/kaldoran/web/blob/master/ujm_emploi_du_temps.php" title="merci">Nicolas Reynaud</a>
+        </p>
+        <p>
+            Voir aussi <a href="https://github.com/Chocobozzz/edt_jean-monnet" title="Voir le dépot GitHub">l'application Android</a>
+        </p>
+    </footer>
 
     
-    <script>
+    <script type="text/javascript">
         var code;
-        
-        function request(callback) {
-            var xhr = new XMLHttpRequest();
-            
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
-                    callback(xhr.responseText);
-                }
-            };
-            
-            xhr.open("GET", "code_edt.cgi", true);
-            xhr.send(null);
-        }
-        
-        function readData(data) {
-           /<p>(.*?)<\/p>/.exec(data);
-           code = RegExp.$1;
-           
-           var img = document.getElementById('image');
-           img.className = img.className.replace("pacman", "");
-           
-           goto(0);
-        }
-        
-        request(readData);
         
         function goto(val) {
             var semaine_obj = document.getElementById('semaine');
@@ -226,6 +219,29 @@
             document.getElementById('image').src = lien;
         }
         
+        function request(callback) {
+            var xhr = new XMLHttpRequest();
+            
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
+                    callback(xhr.responseText);
+                }
+            };
+            
+            xhr.open("GET", "code_edt.cgi", true);
+            xhr.send(null);
+        }
+        
+        function readData(data) {
+           /<p>(.*?)<\/p>/.exec(data);
+           code = RegExp.$1;
+           
+           var img = document.getElementById('image');
+           img.className = img.className.replace("pacman", "");
+           
+           goto(0);
+        }
+        
         var code_promo = document.getElementById('code_promo');
         code_promo.addEventListener("change", function() { goto(0) }, false);
         
@@ -241,10 +257,8 @@
         var semaine_suivante = document.getElementById('semaine_suivante');
         semaine_suivante.addEventListener("click", function() { goto(1) }, false);
         
-        
-        
-
-        
+        request(readData);
+                
     </script>
     
 </body>
